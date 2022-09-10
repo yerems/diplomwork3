a2e4f4c7df07af0d6894ad3d5fb622fb21580aaf
yerems <112744244+yerems@users.noreply.github.com>
2022-09-10 06:21:24 +0300

    Add files via upload

---
+++ scrirt/bxslider.js
@@ -0,0 +1,23 @@
+$(".carusel").slick({
+    infinite:false,
+    prevArrow:$(".prev"),
+    nextArrow:$(".next"),
+    slidesToShow:2,
+    dots:true,
+});
+
+$(".gellery-carusel").slick({
+    infinite:false,
+    prevArrow:$(".prev-gallery"),
+    nextArrow:$(".next-gallery"),
+    slidesToShow:2,
+    dots:true,
+});
+
+$(".reviews-carusel").slick({
+    infinite:false,
+    prevArrow:$(".prev-reviews"),
+    nextArrow:$(".next-reviews"),
+    slidesToShow:1,
+    dots:true,
+});
